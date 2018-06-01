bce193b5aefc296908ab8a089b0e8d08a4a6be44
fmtdevops <devops@fatmactech.com>
2018-06-01 18:25:23 +0530

    Design Uploaded

--- src/app/components/knowledge/knowledge.component.ts
+++ src/app/components/knowledge/knowledge.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit,Inject } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatDialogConfig} from '@angular/material';
 import { CategoryComponent } from '../category/category.component';
 
@@ -9,9 +10,14 @@ import { CategoryComponent } from '../category/category.component';
 })
 export class KnowledgeComponent implements OnInit {
 
+  displayedColumns = ['topic', 'domain', 'off', 'act'];
+  // @ViewChild(MatPaginator) paginator:MatPaginator;
+  dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
+  
   constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
+    // this.dataSource.paginator = this.paginator;
   }
   dialogRef : MatDialogRef<CategoryComponent>;
 
@@ -19,7 +25,7 @@ export class KnowledgeComponent implements OnInit {
   name: string;
   openDialog(): void {
     this.dialogRef = this.dialog.open(CategoryComponent, {
-      width: '800px',
+      width: '90%',
       data: { name: this.name, animal: this.animal }
     });
 
@@ -29,4 +35,37 @@ export class KnowledgeComponent implements OnInit {
     });
  }
 
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
 }
+export interface Element {
+  topic: string;
+  domain: string;
+  office: string;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {topic: "Lorem Ipsum is simply dummy text ", domain: 'IT', office: "Atlanta"},
+  {topic: "Sed ut perspiciatis unde omnis iste natus error", domain: 'Procurement', office: "Boston"},
+  {topic: "Dolor", domain: 'Corporate', office: "Chicago"},
+  {topic: "Sit", domain: 'Project', office: "Washington"},
+  {topic: "Lorem Ipsum is simply dummy text ", domain: 'People', office: "Brooklyn"},
+  {topic: "Lorem", domain: 'IT', office: "Atlanta"},
+  {topic: "Ipsum", domain: 'Procurement', office: "Boston"},
+  {topic: "Dolor", domain: 'Corporate', office: "Chicago"},
+  {topic: "Sit", domain: 'Project', office: "Washington"},
+  {topic: "Lorem Ipsum is simply dummy text ", domain: 'People', office: "Brooklyn"},
+  {topic: "Sed ut perspiciatis unde omnis iste natus error", domain: 'IT', office: "Atlanta"},
+  {topic: "Ipsum", domain: 'Procurement', office: "Boston"},
+  {topic: "consectetur", domain: 'Corporate', office: "Chicago"},
+  {topic: "Sit", domain: 'Project', office: "Washington"},
+  {topic: "Lorem Ipsum is simply dummy text ", domain: 'People', office: "Brooklyn"},
+  {topic: "Lorem", domain: 'IT', office: "Atlanta"},
+  {topic: "Sed ut perspiciatis unde omnis iste natus error", domain: 'Procurement', office: "Boston"},
+  {topic: "Dolor", domain: 'Corporate', office: "Chicago"},
+  {topic: "Sed ut perspiciatis unde omnis iste natus error", domain: 'Project', office: "Washington"},
+  {topic: "Lorem Ipsum is simply dummy text ", domain: 'People', office: "Brooklyn"},
+];
