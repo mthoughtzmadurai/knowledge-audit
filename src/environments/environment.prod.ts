c2654daddf7f5927c7d4eb621f21ebe6e37aeaff
fmtdevops <devops@fatmactech.com>
2018-06-01 17:14:49 +0530

    Design Uploaded

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'https://knowledge-audit.herokuapp.com//api/v1',
+  apiEndpoint: 'https://knowledge-audit.herokuapp.com/api/v1',
   okta: {
     clientId: '0oacrosqllIPmCxXP0h7',
     issuer: 'https://perkinswill.oktapreview.com'
